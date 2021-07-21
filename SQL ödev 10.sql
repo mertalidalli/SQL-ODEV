693d5f4020ddb5c2da29abdd39efcc6aeb7d1a12
mertalidalli <62943845+mertalidalli@users.noreply.github.com>
2021-07-21 16:40:05 +0300

    Add files via upload

---
+++ SQL ödev 10.sql
@@ -0,0 +1,18 @@
+1-city tablosu ile country tablosunda bulunan şehir (city) ve ülke (country) isimlerini birlikte görebileceğimiz LEFT JOIN sorgusunu yazınız.
+
+SELECT city,country FROM city
+LEFT JOIN COUNTRY ON city.country_id=country.country_id
+
+2-customer tablosu ile payment tablosunda bulunan payment_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz
+
+SELECT payment_id,first_name,last_name FROM customer
+RIGHT JOIN payment ON customer.customer_id=payment.customer_id
+
+3- customer tablosu ile rental tablosunda bulunan rental_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz FULL JOIN sorgusunu yazınız.Ü
+
+SELECT rental_id, first_name,last_name FROM customer
+FULL JOIN rental ON rental.customer_id=customer.customer_id
+
+
+
+
